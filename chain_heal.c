9b151a208b4bde8fa0e4c035e1464fced1edf683
WillDBM <willdbm@charter.net>
2025-01-29 22:19:57 -0500

    Initial files needed for chain_heal HW

---
+++ chain_heal.c
@@ -0,0 +1,12 @@
+/*  William Armentrout
+    warmentr
+    chain_heal.c
+    Description:
+*/ 
+
+// Only what we are allowed to use
+#include <studio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
