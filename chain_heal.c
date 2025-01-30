9b644add80b385239623d98556da9c28fe1740f7
William Joseph Armentrout <warmentr@hydra17.eecs.utk.edu>
2025-01-30 08:28:11 -0500

    added struct and error checking

--- chain_heal.c
+++ chain_heal.c
@@ -5,8 +5,30 @@
 */ 
 
 // Only what we are allowed to use
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+
+int init_range, jump_range, num_jumps, init_power;
+double power_reduction;
+
+typedef struct node{
+
+	int x, y;
+	int current_PP, max_PP;
+	struct node *previous;
+
+} Node;
+
+int main(int argc, char *argv[]){
+
+	if (argc != 6) {
+		printf("Wrong Number of arguments");
+		return 0;
+	}
+
+
+	return 0;
+}
